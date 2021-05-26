6c413d6721d67049c13b261faefcf3096fec27e1
Ritik Sethi <58940080+ritiksethi21@users.noreply.github.com>
2021-05-26 09:40:20 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,7 @@
  import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDq2Z1_5iRIfGT92ka3WLou9WLCauTu9XI",
-  authDomain: "instagram-clone-2112.firebaseapp.com",
-  projectId: "instagram-clone-2112",
-  storageBucket: "instagram-clone-2112.appspot.com",
-  messagingSenderId: "162919398271",
-  appId: "1:162919398271:web:23055989f56dd1056af84a",
-  measurementId: "G-C7TRNJRQBK"
+  // your own firebase configuration
 };
   firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
